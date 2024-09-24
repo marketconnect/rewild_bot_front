eacd67fefaea824a7b700d108afbaf9eaa52ae79
marketconnect <ipbogachenko@yandex.ru>
2024-09-24 23:50:31 +0300

    added add group screen

--- lib/presentation/products/seo/all_cards_seo_screen/all_cards_seo_screen.dart
+++ lib/presentation/products/seo/all_cards_seo_screen/all_cards_seo_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-
 import 'package:overlay_loader_with_app_icon/overlay_loader_with_app_icon.dart';
 import 'package:provider/provider.dart';
 import 'package:rewild_bot_front/core/constants/image_constant.dart';
 import 'package:rewild_bot_front/domain/entities/card_of_product_model.dart';
 import 'package:rewild_bot_front/presentation/products/seo/all_cards_seo_screen/all_cards_seo_view_model.dart';
-
 import 'package:rewild_bot_front/widgets/network_image.dart';
 
 class AllCardsSeoScreen extends StatelessWidget {
@@ -27,10 +25,12 @@ class AllCardsSeoScreen extends StatelessWidget {
       appIcon: Image.asset(ImageConstant.imgLogoForLoading),
       child: Scaffold(
         appBar: AppBar(
-            title: const Text('Ваши товары'),
-            scrolledUnderElevation: 2,
-            shadowColor: Colors.black,
-            surfaceTintColor: Colors.transparent),
+          title: const Text('Ваши товары'),
+          scrolledUnderElevation: 2,
+          shadowColor: Colors.black,
+          surfaceTintColor: Colors.transparent,
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
         body: apikeyExists || isLoading
             ? ListView.builder(
                 padding: const EdgeInsets.all(8.0),
@@ -43,9 +43,7 @@ class AllCardsSeoScreen extends StatelessWidget {
                       if (cardContent == null) return;
                       goToSeoToolScreen(product: product, card: cardContent);
                     },
-                    child: ProductCard(
-                      productCard: product,
-                    ),
+                    child: ProductCard(productCard: product),
                   );
                 },
               )
@@ -60,7 +58,8 @@ class AllCardsSeoScreen extends StatelessWidget {
                 },
                 backgroundColor: Theme.of(context).primaryColor,
                 foregroundColor: Colors.white,
-                child: const Icon(Icons.no_encryption_gmailerrorred_outlined))
+                child: const Icon(Icons.category),
+              )
             : null,
       ),
     );
@@ -79,11 +78,12 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(12),
       ),
-      elevation: 3,
+      elevation: 4,
+      margin: const EdgeInsets.symmetric(vertical: 10),
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(12.0),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -105,24 +105,28 @@ class ProductCard extends StatelessWidget {
                       color: Theme.of(context).colorScheme.onSurface,
                     ),
                   ),
+                  const SizedBox(height: 4),
+                  Text(
+                    "Категория: ${productCard.name}",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
                   const SizedBox(height: 8),
-                  // Row(
-                  //   children: [
-                  //     const Icon(
-                  //       Icons.star,
-                  //       color: Colors.amber,
-                  //       size: 20,
-                  //     ),
-                  //     const SizedBox(width: 4),
-                  //     Text(
-                  //       productCard.rating.toString(),
-                  //       style: TextStyle(
-                  //         fontSize: 16,
-                  //         color: Theme.of(context).colorScheme.onSurface,
-                  //       ),
-                  //     ),
-                  //   ],
-                  // ),
+                  Row(
+                    children: [
+                      Icon(Icons.star, color: Colors.amber, size: 20),
+                      const SizedBox(width: 4),
+                      Text(
+                        productCard.rating.toString(),
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Theme.of(context).colorScheme.onSurface,
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
@@ -162,36 +166,35 @@ class ApiKeyMissingWidget extends StatelessWidget {
           Text(
             'API ключ не добавлен',
             textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                  color: Theme.of(context).primaryColor,
-                ),
+            // style: Theme.of(context).textTheme.titleLarge.copyWith(
+            //       color: Theme.of(context).primaryColor,
+            //       fontWeight: FontWeight.bold,
+            //     ),
           ),
           const SizedBox(height: 8),
           Text(
-            'Для подключения к API портала продавца, вы можете добавить API токен. ',
+            'Для подключения к API портала продавца, добавьте API токен или воспользуйтесь генерацией SEO без API.',
             textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyMedium,
+            style: Theme.of(context).textTheme.bodyLarge,
           ),
           const SizedBox(height: 24),
           ElevatedButton.icon(
             onPressed: onAddApiKeyPressed,
-            // icon: const Icon(Icons.vpn_key),
+            icon: const Icon(Icons.vpn_key),
             label: const Text('Добавить API токен'),
             style: ElevatedButton.styleFrom(
               backgroundColor: Theme.of(context).primaryColor,
-              foregroundColor: Colors.white,
-              padding: EdgeInsets.symmetric(
-                  vertical: MediaQuery.of(context).size.height * 0.05),
+              padding: const EdgeInsets.symmetric(vertical: 16),
             ),
           ),
           const SizedBox(height: 16),
           OutlinedButton.icon(
             onPressed: onSeoByCategoryPressed,
-            label: const Text('Без API'),
+            icon: const Icon(Icons.category),
+            label: const Text('SEO по категориям'),
             style: OutlinedButton.styleFrom(
               side: BorderSide(color: Theme.of(context).primaryColor),
-              padding: EdgeInsets.symmetric(
-                  vertical: MediaQuery.of(context).size.height * 0.05),
+              padding: const EdgeInsets.symmetric(vertical: 16),
             ),
           ),
         ],
